61972284599e101fe53c03d7e0bd9c3796e8570d
Derek Delaney <derek.delaney@nike.com>
2018-11-02 15:55:29 -0700

    rewrote save and load options

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
+/* Defaults */
 const defaultTasks = [];
-
 const defaultDefaults = [
     { day: 'Mon', value: '8', checked: true },
     { day: 'Tue', value: '8', checked: true },
@@ -9,77 +9,60 @@ const defaultDefaults = [
     { day: 'Sat', value: '0', checked: true },
     { day: 'Sun', value: '0', checked: true },
 ];
+const defaultNotifications = true;
+const defaultNotificationTime = '11:00';
+const defaultReminderTime = 14400000; // 4 hours
 
-chrome.storage.sync.get(['tasks', 'defaultDays'], function({
+/* init load */
+chrome.storage.sync.get(
+    [
+        'tasks',
+        'defaultDays',
+        'notifications',
+        'notificationTime',
+        'reminderTime',
+    ],
+    function({
+        tasks = defaultTasks,
+        defaultDays = defaultDefaults,
+        notifications = defaultNotifications,
+        notificationTime = defaultNotificationTime,
+        reminderTime = defaultReminderTime,
+    }) {
+        chrome.storage.sync.set(
+            {
+                tasks,
+                defaultDays,
+                notifications,
+                notificationTime,
+                reminderTime,
+            },
+            function() {
+                generateAll(
+                    tasks,
+                    defaultDays,
+                    notifications,
+                    notificationTime,
+                    reminderTime
+                );
+            }
+        );
+    }
+);
+
+/* GENERATE AND POPULATE HTML FUNCTIONS */
+function generateAll(
     tasks,
     defaultDays,
-}) {
-    if (Array.isArray(tasks)) {
-        generateTaskRows(tasks);
-    } else {
-        chrome.storage.sync.set({ tasks: defaultTasks }, function() {
-            generateTaskRows(defaultTasks);
-        });
-    }
-    if (Array.isArray(defaultDays)) {
-        populateDefaultDays(defaultDays);
-    } else {
-        chrome.storage.sync.set({ defaultDays: defaultDefaults }, function() {
-            populateDefaultDays(defaultDefaults);
-        });
-    }
-});
-
-/* GET & SAVE FUNCTIONS */
-function getTasks(func) {
-    chrome.storage.sync.get('tasks', function(data) {
-        let { tasks } = data;
-        if (!Array.isArray(tasks)) {
-            tasks = [];
-        }
-        func(tasks);
-    });
+    notifications,
+    notificationTime,
+    reminderTime
+) {
+    generateTaskRows(tasks);
+    populateDefaultDays(defaultDays);
+    setNotificationArea(notifications, notificationTime, reminderTime);
 }
 
-function saveTask(task) {
-    if (!task) return;
-    chrome.storage.sync.get('tasks', function(data) {
-        let { tasks } = data;
-        if (!Array.isArray(tasks)) {
-            tasks = [];
-        }
-        tasks.push({ name: task, ratio: 0 });
-        chrome.storage.sync.set({ tasks }, function() {
-            generateTaskRows(tasks);
-        });
-    });
-}
-
-function saveTasks(tasks) {
-    if (!Array.isArray(tasks)) return;
-    chrome.storage.sync.set({ tasks }, function() {
-        generateTaskRows(tasks);
-    });
-}
-
-function getDefaultDays(func) {
-    chrome.storage.sync.get('defaultDays', function(data) {
-        let { defaultDays } = data;
-        if (!Array.isArray(defaultDays)) {
-            defaultDays = [];
-        }
-        func(defaultDays);
-    });
-}
-
-function saveDefaultDays(defaultDays) {
-    if (!Array.isArray(defaultDays)) return;
-    chrome.storage.sync.set({ defaultDays }, function() {
-        populateDefaultDays(defaultDays);
-    });
-}
-
-/* GENERATE HTML FUNCTIONS */
 function generateTaskRows(tasks) {
     const tasksTable = document.getElementById('tasks');
     tasksTable.innerHTML = tasks
@@ -87,7 +70,9 @@ function generateTaskRows(tasks) {
             obj =>
                 `<tr>
                     <td>
-                        ${obj.name}
+                        <input type="text" readonly class="form-control-plaintext task-names" value="${
+                            obj.name
+                        }" tabindex="-1">
                     </td>
                     <td>
                         <input type="text" class="form-control ratio-values" value="${
@@ -101,10 +86,16 @@ function generateTaskRows(tasks) {
 
 function populateDefaultDays(defaults) {
     const days = document.getElementById('default-days-row');
-    days.innerHTML = defaults
-        .map(obj => `<th scope="col">${obj.day}</th>`)
-        .join('\n');
     const value = document.getElementById('default-value-row');
+    const checked = document.getElementById('default-checked-row');
+    days.innerHTML = defaults
+        .map(
+            obj =>
+                `<th scope="col"><input type="text" readonly class="form-control-plaintext default-days" value="${
+                    obj.day
+                }" tabindex="-1"></th>`
+        )
+        .join('\n');
     value.innerHTML = defaults
         .map(
             obj =>
@@ -113,7 +104,6 @@ function populateDefaultDays(defaults) {
                 }"></td>`
         )
         .join('\n');
-    const checked = document.getElementById('default-checked-row');
     checked.innerHTML = defaults
         .map(
             obj =>
@@ -122,6 +112,20 @@ function populateDefaultDays(defaults) {
                 }></td>`
         )
         .join('\n');
+}
+
+function setNotificationArea(notifications, notificationTime, reminderTime) {
+    const notificationSwitch = document.getElementById('notification-switch');
+    const notificationTimeElement = document.getElementById(
+        'notification-time'
+    );
+    const reminderTimeElement = document.getElementById('reminder-time');
+
+    showHideNotification(notifications);
+
+    notificationSwitch.checked = notifications;
+    notificationTimeElement.value = notificationTime;
+    reminderTimeElement.value = reminderTime;
 }
 
 /* CREATE NEW TASK NAME */
@@ -135,90 +139,27 @@ inputNewTaskName.onkeydown = e => {
 };
 function addTaskName() {
     const task = inputNewTaskName.value.trim().toLowerCase();
-    saveTask(task);
+    addTaskRow(task);
     inputNewTaskName.value = '';
 }
 
-/* SAVE OPTIONS */
-const saveOptions = document.getElementById('saveOptions');
-saveOptions.onclick = () => {
-    const taskRatios = document.querySelectorAll('.ratio-values');
-    const defaultValue = document.querySelectorAll('.default-value');
-    const defaultChecked = document.querySelectorAll('.default-checked');
-    getTasks(tasks => {
-        if (tasks.length !== taskRatios.length) {
-            console.error('Something went wrong. Can not save Tasks.');
-        } else {
-            const updatedTasks = tasks.map((t, i) => ({
-                name: t.name,
-                ratio: taskRatios[i].value,
-            }));
-            saveTasks(updatedTasks);
-        }
-    });
-    getDefaultDays(defaultDays => {
-        if (defaultDays.length !== defaultValue.length) {
-            console.error('Something went wrong. Can not save defaults.');
-        } else {
-            const updatedDefaults = defaultDays.map((d, i) => ({
-                day: d.day,
-                value: defaultValue[i].value,
-                checked: defaultChecked[i].checked,
-            }));
-            saveDefaultDays(updatedDefaults);
-        }
-    });
-    saveNotificationSettings();
-};
+function addTaskRow(taskName) {
+    const tasksTable = document.getElementById('tasks');
+    const row = document.createElement('tr');
+    row.innerHTML = `<td>
+            <input type="text" readonly class="form-control-plaintext task-names" value="${taskName}" tabindex="-1">
+        </td>
+        <td>
+            <input type="text" class="form-control ratio-values" value="0">
+        </td>`;
+    tasksTable.appendChild(row);
+}
 
-/* RESET OPTIONS */
-const resetOptions = document.getElementById('resetOptions');
-resetOptions.onclick = () => {
-    chrome.storage.sync.set({ tasks: defaultTasks }, function() {
-        generateTaskRows(defaultTasks);
-    });
-    chrome.storage.sync.set({ defaultDays: defaultDefaults }, function() {
-        populateDefaultDays(defaultDefaults);
-    });
-};
-
-/* NOTIFICATIONS */
+/* TOGGLE NOTIFICATIONS */
 const notificationSwitch = document.getElementById('notification-switch');
-const notificationTimeElement = document.getElementById('notification-time');
-const reminderTimeElement = document.getElementById('reminder-time');
-
-chrome.storage.sync.get(
-    ['notifications', 'notificationTime', 'reminderTime'],
-    function({ notifications, notificationTime, reminderTime }) {
-        notificationSwitch.checked = !!notifications;
-        showHideNotification(!!notifications);
-        if (!notificationTime) {
-            notificationTimeElement.value = '11:00';
-        } else {
-            notificationTimeElement.value = notificationTime;
-        }
-        if (!reminderTime) {
-            reminderTimeElement.value = '14400000';
-        } else {
-            reminderTimeElement.value = reminderTime;
-        }
-    }
-);
-
 notificationSwitch.onchange = function({ target: { checked } }) {
     showHideNotification(checked);
 };
-
-function saveNotificationSettings() {
-    const checked = notificationSwitch.checked;
-    const time = notificationTimeElement.value;
-    const reminder = reminderTimeElement.value;
-    chrome.storage.sync.set({
-        notifications: checked,
-        notificationTime: time,
-        reminderTime: reminder,
-    });
-}
 
 function showHideNotification(bool) {
     if (bool) {
@@ -231,3 +172,97 @@ function showHideNotification(bool) {
             .forEach(e => e.classList.add('hidden'));
     }
 }
+
+/* SAVE OPTIONS */
+const saveOptions = document.getElementById('saveOptions');
+saveOptions.onclick = () => {
+    const taskNames = document.querySelectorAll('.task-names');
+    const taskRatios = document.querySelectorAll('.ratio-values');
+    const defaultDaysElements = document.querySelectorAll('.default-days');
+    const defaultValue = document.querySelectorAll('.default-value');
+    const defaultChecked = document.querySelectorAll('.default-checked');
+    const notificationSwitch = document.getElementById('notification-switch');
+    const notificationTimeElement = document.getElementById(
+        'notification-time'
+    );
+    const reminderTimeElement = document.getElementById('reminder-time');
+
+    const errors = [];
+    let options = {};
+
+    // tasks and ratios
+    if (taskNames.length !== taskRatios.length) {
+        errors.push('Invalid HTML for Tasks and Ratios');
+    } else {
+        const tasks = [];
+        for (let i = 0; i < taskNames.length; i++) {
+            tasks.push({
+                name: taskNames[i].value,
+                ratio: taskRatios[i].value,
+            });
+        }
+        options = {
+            ...options,
+            tasks,
+        };
+    }
+
+    // default days
+    if (
+        defaultDaysElements.length !== defaultValue.length &&
+        defaultValue.length !== defaultChecked.length
+    ) {
+        errors.push('Invalid HTML for Default Days');
+    } else {
+        const defaultDays = [];
+        for (let i = 0; i < defaultDaysElements.length; i++) {
+            defaultDays.push({
+                day: defaultDaysElements[i].value,
+                value: defaultValue[i].value,
+                checked: defaultChecked[i].checked,
+            });
+        }
+        options = {
+            ...options,
+            defaultDays,
+        };
+    }
+
+    // notifications
+    const notifications = notificationSwitch.checked;
+    const notificationTime = notificationTimeElement.value;
+    const reminderTime = reminderTimeElement.value;
+    options = {
+        ...options,
+        notifications,
+        notificationTime,
+        reminderTime,
+    };
+
+    chrome.storage.sync.set(options, () => {
+        if (chrome.runtime.lastError) {
+            if (chrome.runtime.lastError.message) {
+                errors.push(message);
+            } else {
+                errors.push('An unknown error has occurred.');
+            }
+        }
+        if (errors.length) {
+            //display error message
+        } else {
+            // display success message
+        }
+    });
+};
+
+/* RESET OPTIONS */
+const resetOptions = document.getElementById('resetOptions');
+resetOptions.onclick = () => {
+    generateAll(
+        defaultTasks,
+        defaultDefaults,
+        defaultNotifications,
+        defaultNotificationTime,
+        defaultReminderTime
+    );
+};
