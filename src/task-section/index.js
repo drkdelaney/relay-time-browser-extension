b0fecc40124f81a51af1c1939ef404ec9c9d2e69
Derek Delaney <derek.delaney@nike.com>
2019-05-20 23:38:55 -0700

    downloads current tasks

--- src/task-section/index.js
+++ src/task-section/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import XLSX from 'xlsx';
 import TaskTable from './task-table';
 import { ReactComponent as ExcelUp } from '../excel-upload.svg';
 import { ReactComponent as ExcelDown } from '../excel-download.svg';
+import { get } from '../storage-helper';
 
 class TaskSection extends Component {
     handleUpload = () => {}
 
-    handleDownload = () => {}
+    handleDownload = async () => {
+        const { tasks } = await get('tasks');
+        const arrayTasks = tasks.map(a => ([a.name, a.ratio]));
+        const data = [
+            ['Activity', 'Ratio'],
+            ...arrayTasks
+        ];
+        const worksheet = XLSX.utils.aoa_to_sheet(data);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'timesheets');
+        XLSX.writeFile(workbook, 'relay_time.xlsx');
+    }
 
     render() {
         return (
